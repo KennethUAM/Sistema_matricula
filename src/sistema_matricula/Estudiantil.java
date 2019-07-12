1da0153bd0de00a6b761d2ec368589b436002e3f
Kenneth <Kenneth@LAPTOPKENNETH>
2019-07-12 15:39:12 -0600

    Sistema_estudiantil

---
+++ src/sistema_matricula/Estudiantil.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema_matricula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Kenneth
+ */
+public class Estudiantil extends Persona {
+
+    private List<Persona> estudiantes = new ArrayList<Persona>();
+    private double promedio;
+    private int nota1, nota2, nota3;
+
+    public Estudiantil() {
+    }
+
+    public Estudiantil(double promedio, int nota1, int nota2, int nota3, String nombre, String primero_apellido, String segundo_apellido, int cedula, int edad, boolean sexo) {
+        super(nombre, primero_apellido, segundo_apellido, cedula, edad, sexo);
+        this.promedio = promedio;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public List<Persona> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(List<Persona> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public int getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public int getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
+
+    public int getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(int nota3) {
+        this.nota3 = nota3;
+    }
+
+}
